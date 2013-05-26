1c21a311cc3e969d1fab7389929e71caae941557
Eric Johnson <eric.git@iijo.org>
2013-05-26 11:50:47 +0100

    initial commit

---
+++ lib/Plack/Middleware/Redirect.pm
@@ -0,0 +1,39 @@
+package Plack::Middleware::Redirect;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Plack::Middleware::Redirect - It's new $module
+
+=head1 SYNOPSIS
+
+    use Plack::Middleware::Redirect;
+
+=head1 DESCRIPTION
+
+Plack::Middleware::Redirect is ...
+
+=head1 LICENSE
+
+Copyright (C) Eric Johnson.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Eric Johnson E<lt>eric.git@iijo.orgE<gt>
+
+=cut
+
